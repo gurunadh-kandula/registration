ec68df4f093f1ff74d4aba5152bc3dfecdb2159d
Anubhav Narwal <Anubhav.Narwal@jda.com>
2018-08-20 13:57:13 +0000

    mail api   implemented and transaction

--- src/main/java/com/jda/user/controller/ResetPasswordController.java
+++ src/main/java/com/jda/user/controller/ResetPasswordController.java
@@ -7,14 +7,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.jda.user.model.Login;
-import com.jda.user.model.ResetPassword;
 import com.jda.user.model.User;
 import com.jda.user.service.UserService;
 
--- src/main/java/com/jda/user/service/UserServiceImpl.java
+++ src/main/java/com/jda/user/service/UserServiceImpl.java
@@ -10,6 +10,8 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.jda.user.model.ForgotPassword;
 import com.jda.user.model.Login;
 import com.jda.user.model.ResetPassword;
@@ -20,12 +22,13 @@ public class UserServiceImpl implements  UserService{
 	  DataSource datasource;
 	  @Autowired
 	  JdbcTemplate jdbcTemplate;
-		
+	  @Transactional
 	  public void register(User user) {
 	    String sql = "insert into users values(?,?,?,?,?,?,?)";
 	    jdbcTemplate.update(sql, new Object[] { user.getUsername(), user.getPassword(), user.getFirstname(),
 	    user.getLastname(), user.getEmail(), user.getAddress(), user.getPhone() });
 	    }
+	  
 	    public User validateUser(Login login) {
 	   String sql = "select * from users where username='" + login.getUsername() + "' and password='" + login.getPassword()
 	    + "'";
@@ -38,6 +41,7 @@ public class UserServiceImpl implements  UserService{
 	 	    List<User> users = jdbcTemplate.query(sql, new UserMapper());
 	 	    return users.size() > 0 ? users.get(0) : null;
 	 	    }
+	    @Transactional
 	   public void newPassword(String password,String token) {
 	   	String sql="update users set password='"+password +"'  where token='"+token+"'";
 			jdbcTemplate.update(sql);
